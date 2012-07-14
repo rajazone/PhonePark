df2d49d061a1a8ae2c22732105aeaa8314179318
Raja <rajazone@gmail.com>
2012-07-14 01:12:46 -0500

    Update master

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-	$connect=mysql_connect("socioeventscom.ipagemysql.com","rajakrish","pipeline45")
+	$connect=mysql_connect("host","username","password")
 	or
 	die("A connection to the server could not be established");
-	$result=mysql_select_db("phonepark")
+	$result=mysql_select_db("dbname")
 	or 
 	die("Database could not be selected");
 	?>
